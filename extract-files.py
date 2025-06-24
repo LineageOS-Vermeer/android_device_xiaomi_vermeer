9a20f26e8007946173cf4376c6ec0f257c764d2e
GioKas93 <giokas93@pm.me>
2025-06-24 15:29:11 +0200

    vermeer: Switch to python extract-utils

---
+++ extract-files.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/sm8550-common',
+    'hardware/qcom-caf/sm8550',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/xiaomi/sm8550-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'odm/etc/camera/aivsModel_6C06C006',
+        'odm/lib64/libmialgo_aisn.so',
+    ): blob_fixup()
+        .split_bytes(20 * 1024 * 1024),  # 20MB chunks
+    (
+        'odm/etc/camera/enhance_motiontuning.xml',
+        'odm/etc/camera/night_motiontuning.xml',
+        'odm/etc/camera/motiontuning.xml',
+    ): blob_fixup()
+        .regex_replace(r'<\?xml=', '<?xml '),
+    (
+        'odm/lib64/libwrapper_dlengine.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
+    (
+        'odm/lib64/libailab_rawhdr.so',
+        'odm/lib64/libxmi_high_dynamic_range_cdsp.so',
+    ): blob_fixup()
+        .strip_debug_sections(),
+}
+
+module = ExtractUtilsModule(
+    'vermeer',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8550-common', module.vendor
+    )
+    utils.run()
